d8c09b8991f6dd0347c76a6cf1356e3470e536dd
MacRimi <123239993+MacRimi@users.noreply.github.com>
2024-05-09 20:02:58 +0200

    Update steam-appimage-plus.sh

--- scripts/steam-appimage-plus.sh
+++ scripts/steam-appimage-plus.sh
@@ -81,33 +81,40 @@ fi
 
 # Añadir el sistema "steam" y "ajustes" a es_systems.cfg solo si no están ya configurados
 # Verificar si el sistema "ajustes" ya existe
-if ! grep -q '<name>ajustes</name>' "$ES_SYSTEMS_CFG"; then
-    cat <<EOF >> "$ES_SYSTEMS_CFG"
+# Si se encontró es_systems.cfg, añadir configuraciones con permisos administrativos
+if [[ -n "$ES_SYSTEMS_CFG" ]]; then
+    # Añadir el sistema "ajustes" solo si no está ya configurado
+    if ! grep -q '<name>ajustes</name>' "$ES_SYSTEMS_CFG"; then
+        sudo bash -c "cat <<EOF >> $ES_SYSTEMS_CFG
 <system>
     <name>ajustes</name>
     <fullname>Configuraciones</fullname>
-    <path>$HOME/RetroPie/roms/ajustes</path>
+    <path>$HOME_DIR/RetroPie/roms/ajustes</path>
     <extension>.sh</extension>
     <command>%ROM%</command>
     <platform>config</platform>
     <theme>ajustes</theme>
 </system>
-EOF
-fi
+EOF"
+    fi
 
-# Verificar si el sistema "steam" ya existe
-if ! grep -q '<name>steam</name>' "$ES_SYSTEMS_CFG"; then
-    cat <<EOF >> "$ES_SYSTEMS_CFG"
+    # Añadir el sistema "steam" solo si no está ya configurado
+    if ! grep -q '<name>steam</name>' "$ES_SYSTEMS_CFG"; then
+        sudo bash -c "cat <<EOF >> $ES_SYSTEMS_CFG
 <system>
     <name>steam</name>
     <fullname>Steam</fullname>
-    <path>$HOME/RetroPie/roms/steam</path>
+    <path>$HOME_DIR/RetroPie/roms/steam</path>
     <extension>.sh</extension>
     <command>%ROM%</command>
     <platform>pc</platform>
     <theme>steam</theme>
 </system>
-EOF
+EOF"
+    fi
+else
+    echo "No se encontró es_systems.cfg."
+    exit 1
 fi
 
 # Agregar script para lanzar Steam en Big Picture al directorio "ajustes" solo si no existe
