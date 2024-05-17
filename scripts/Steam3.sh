311592a95ca01e4191d7f9f53785563758d0e102
MacRimi <123239993+MacRimi@users.noreply.github.com>
2024-05-17 11:48:04 +0200

    Update Steam3.sh

--- scripts/Steam3.sh
+++ scripts/Steam3.sh
@@ -15,21 +15,21 @@ rp_module_licence="Proprietary https://store.steampowered.com/subscriber_agreeme
 rp_module_section="exp"
 rp_module_flags="!all 64bit"
 
-function depends_steam() {
-    local depends=(curl wget)
-    getDepends "${depends[@]}"
-    
-    sudo dpkg --add-architecture i386
-
-    sudo apt update
-
-    sudo apt install -y libc6:amd64 libc6:i386 \
-                        libegl1:amd64 libegl1:i386 \
-                        libgbm1:amd64 libgbm1:i386 \
-                        libgl1-mesa-glx:amd64 libgl1-mesa-glx:i386 \
-                        libgl1-mesa-dri:amd64 libgl1-mesa-dri:i386 \
-                        steam-libs-amd64:amd64 steam-libs-i386:i386
-}
+#function depends_steam() {
+#    local depends=(curl wget)
+#    getDepends "${depends[@]}"
+#    
+#    sudo dpkg --add-architecture i386
+#
+#    sudo apt update
+#
+#    sudo apt install -y libc6:amd64 libc6:i386 \
+#                        libegl1:amd64 libegl1:i386 \
+#                        libgbm1:amd64 libgbm1:i386 \
+#                        libgl1-mesa-glx:amd64 libgl1-mesa-glx:i386 \
+#                        libgl1-mesa-dri:amd64 libgl1-mesa-dri:i386 \
+#                        steam-libs-amd64:amd64 steam-libs-i386:i386
+#}
 
 function install_bin_steam() {
     mkdir -p "$md_inst/bin"
