3dc5276959bf89994e3c9137be1f0553684e9f98
MacRimi <123239993+MacRimi@users.noreply.github.com>
2024-05-17 17:35:41 +0200

    Update steam7.sh

--- scripts/steam7.sh
+++ scripts/steam7.sh
@@ -19,13 +19,10 @@ function depends_steam() {
     local depends=(curl wget)
     getDepends "${depends[@]}"
 
-    echo "Agregando arquitectura i386..."
     sudo dpkg --add-architecture i386
 
-    echo "Actualizando lista de paquetes..."
     sudo apt update
 
-    echo "Instalando dependencias..."
     sudo apt install -y libc6:amd64 libc6:i386 \
                         libegl1:amd64 libegl1:i386 \
                         libgbm1:amd64 libgbm1:i386 \
@@ -36,37 +33,14 @@ function depends_steam() {
 
 function install_bin_steam() {
     mkdir -p "$md_inst/bin"
-    echo "Descargando steam_latest.deb..."
-    wget --content-disposition https://steamcdn-a.akamaihd.net/client/installer/steam_latest.deb -O "$md_inst/bin/steam_latest.deb"
-    if [ $? -ne 0 ]; then
-        echo "Error: Fallo al descargar steam_latest.deb"
-        return 1
-    fi
-
-    echo "Instalando steam_latest.deb..."
-    sudo dpkg -i "$md_inst/bin/steam_latest.deb" || sudo apt -f install -y
-    if [ $? -ne 0 ]; then
-        echo "Error: Fallo al instalar steam_latest.deb"
-        return 1
-    fi
-
-    rm "$md_inst/bin/steam_latest.deb"
+    wget --content-disposition https://cdn.cloudflare.steamstatic.com/client/installer/steam.deb -O "$md_inst/bin/steam.deb"
+    sudo dpkg -i "$md_inst/bin/steam.deb" || sudo apt -f install -y
+    rm "$md_inst/bin/steam.deb"
     chmod +x "$md_inst/bin/steam"
 }
 
 function configure_steam() {
     mkRomDir "steam"
     addEmulator 1 "$md_id" "steam" "$md_inst/bin/steam %ROM%"
-    addSystem "steam"
-
-    # Agregar nueva entrada al archivo es_systems.cfg
-    echo "<system>" >> /etc/emulationstation/es_systems.cfg
-    echo "    <name>steam</name>" >> /etc/emulationstation/es_systems.cfg
-    echo "    <fullname>Steam</fullname>" >> /etc/emulationstation/es_systems.cfg
-    echo "    <path>$HOME/RetroPie/roms/steam</path>" >> /etc/emulationstation/es_systems.cfg
-    echo "    <extension>.sh .SH</extension>" >> /etc/emulationstation/es_systems.cfg
-    echo "    <command>%ROM%</command>" >> /etc/emulationstation/es_systems.cfg
-    echo "    <platform>steam</platform>" >> /etc/emulationstation/es_systems.cfg
-    echo "    <theme>steam</theme>" >> /etc/emulationstation/es_systems.cfg
-    echo "</system>" >> /etc/emulationstation/es_systems.cfg
+    addSystem "steam" ".sh .SH"
 }
