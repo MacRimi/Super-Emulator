6725b4f27e926805b35074ea23e60faba089ede3
MacRimi <123239993+MacRimi@users.noreply.github.com>
2024-05-18 16:29:13 +0200

    Update and rename steam.sh to steam-AppImage.sh

--- scripts/steam-AppImage.sh
+++ scripts/steam-AppImage.sh
@@ -1,51 +1,49 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Define el directorio base del usuario estándar
-HOME_DIR=$(eval echo ~$SUDO_USER)
+# This file is part of The RetroPie Project
+#
+# The RetroPie Project is the legal property of its developers, whose names are
+# too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
+#
+# See the LICENSE.md file at the top-level directory of this distribution and
+# at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
 
-# Define mkRomDir para crear directorios si no está ya definido
-mkRomDir() {
-    mkdir -p "$1"
+rp_module_id="steam"
+rp_module_desc="Steam - Plataforma de distribución de videojuegos"
+rp_module_help="Instrucciones: Este script instalará Steam en tu sistema. Para lanzar Steam, utiliza el acceso directo en RetroPie."
+rp_module_licence="Proprietary https://store.steampowered.com/subscriber_agreement/"
+rp_module_section="exp"
+rp_module_flags="!all 64bit"
+
+function depends_steam() {
+    local depends=(curl wget)
+    getDepends "${depends[@]}"
+
+    sudo dpkg --add-architecture i386
+
+    sudo apt update
+
+    sudo apt install -y libc6:amd64 libc6:i386 \
+                        libegl1:amd64 libegl1:i386 \
+                        libgbm1:amd64 libgbm1:i386 \
+                        libgl1-mesa-glx:amd64 libgl1-mesa-glx:i386 \
+                        libgl1-mesa-dri:amd64 libgl1-mesa-dri:i386 \
+                        steam-libs-amd64:amd64 steam-libs-i386:i386
 }
 
-# Crear el directorio para instalar Steam
-mkRomDir "$HOME_DIR/RetroPie/roms/steam"
+function install_bin_steam() {
+    mkdir -p "$md_inst/bin"
+    wget -O steam.deb https://cdn.cloudflare.steamstatic.com/client/installer/steam.deb
+    sudo apt install -y ./steam.deb || sudo apt -f install -y
+    rm steam.deb
+}
 
-# Crear el directorio "ajustes"
-mkRomDir "$HOME_DIR/RetroPie/roms/ajustes"
+function configure_steam() {
+    mkRomDir "steam"
+    addEmulator 1 "$md_id" "steam"
+    addSystem "steam"
 
-# Descargar e instalar Steam
-wget --content-disposition "https://cdn.cloudflare.steamstatic.com/client/installer/steam.deb" -O "$HOME_DIR/RetroPie/roms/steam/steam.deb"
-sudo apt-get install -y "$HOME_DIR/RetroPie/roms/steam/steam.deb"
-rm "$HOME_DIR/RetroPie/roms/steam/steam.deb"  # Eliminar el archivo después de instalar
-
-# Ruta a es_systems.cfg
-ES_SYSTEMS_CFG="/etc/emulationstation/es_systems.cfg"
-
-# Agregar sistemas a es_systems.cfg
-# Agregar el sistema "ajustes" antes de </systemList>
-sudo sed -i "/<\/systemList>/i \
-<system>\
-    <name>ajustes</name>\
-    <fullname>Configuraciones</fullname>\
-    <path>$HOME_DIR/RetroPie/roms/ajustes</path>\
-    <extension>.sh</extension>\
-    <command>%ROM%</command>\
-    <platform>config</platform>\
-    <theme>ajustes</theme>\
-</system>" "$ES_SYSTEMS_CFG"
-
-# Agregar el sistema "steam" antes de </systemList>
-sudo sed -i "/<\/systemList>/i \
-<system>\
-    <name>steam</name>\
-    <fullname>Steam</fullname>\
-    <path>$HOME_DIR/RetroPie/roms/steam</path>\
-    <extension>.sh</extension>\
-    <command>%ROM%</command>\
-    <platform>pc</platform>\
-    <theme>steam</theme>\
-</system>" "$ES_SYSTEMS_CFG"
+}
 
 ####################################################
 # crear y añadir scripts
