c7aa06ced59bd5b24cb573bd88c5b62e1a2e29d7
MacRimi <123239993+MacRimi@users.noreply.github.com>
2024-05-18 20:43:22 +0200

    Update and rename menu555555.sh to menu5555555.sh

--- scripts/menu5555555.sh
+++ scripts/menu5555555.sh
@@ -147,7 +147,8 @@ function ajustes_emuladores() {
 
 if ! grep -q '<name>ajustes</name>' "$es_systems_cfg"; then
     # Definir el nuevo sistema
-    nuevo_sistema="  <system>
+    nuevo_sistema="
+  <system>
     <name>ajustes</name>
     <fullname>Configuraciones</fullname>
     <path>/root/RetroPie/roms/ajustes</path>
