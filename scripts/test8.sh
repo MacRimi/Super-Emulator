7245beda1d74e1e622d474f39b6e07645a981ff5
MacRimi <123239993+MacRimi@users.noreply.github.com>
2024-05-20 17:45:12 +0200

    Update test9.sh

--- scripts/test8.sh
+++ scripts/test8.sh
@@ -4,9 +4,32 @@ REPO_URL="https://github.com/MacRimi/Super-RetroPie"
 GLOBAL_INSTALL_DIR="/opt/Super-RetroPie"
 USER_HOME=$(eval echo ~$USER)
 USER_INSTALL_DIR="$USER_HOME/Super-RetroPie"
+SCRIPT_NAME="super-retropie.sh"
+VERSION_FILE_NAME="version.txt"
 TMP_DIR=$(mktemp -d)
 
-# Determinar los directorios y archivos en función de la instalación de RetroPie
+# Asegurarse de que el script se ejecute con permisos de superusuario
+if [ "$EUID" -ne 0 ]; then
+  echo "Por favor, ejecute este script como root."
+  exit 1
+fi
+
+# Función para instalar dependencias
+install_dependencies() {
+    local dependencies=("git" "wget" "dialog" "expect" "lvm2")
+    echo "Verificando e instalando dependencias necesarias..."
+    for pkg in "${dependencies[@]}"; do
+        if ! command -v $pkg &> /dev/null; then
+            echo "Instalando $pkg..."
+            apt-get install -y $pkg
+        fi
+    done
+}
+
+# Instalar dependencias antes de proceder
+install_dependencies
+
+# Comprobación de RetroPie instalado y configuración de directorios
 if command -v emulationstation &> /dev/null; then
     INSTALL_DIR="$GLOBAL_INSTALL_DIR"
     echo "RetroPie está instalado. Usando el directorio global: $INSTALL_DIR"
