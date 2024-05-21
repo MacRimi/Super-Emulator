8401fb3e586d8c5f2f44df69e5a75cba9653508f
MacRimi <123239993+MacRimi@users.noreply.github.com>
2024-05-21 19:38:43 +0200

    Update run3.sh

--- scripts/run3.sh
+++ scripts/run3.sh
@@ -15,14 +15,9 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 # Descargar y ejecutar el script si emulationstation no está instalado
-if ! command -v emulationstation_test &> /dev/null; then
-  echo "emulationstation no está instalado. Descargando y ejecutando $SCRIPT_NAME..."
+if ! command -v emulationstation &> /dev/null; then
   mkdir -p "$USER_INSTALL_DIR"
   wget -q "$REPO_URL" -O "$USER_SCRIPT_PATH"
-  if [ $? -ne 0 ]; then
-    echo "Error al descargar $SCRIPT_NAME en $USER_INSTALL_DIR."
-    exit 1
-  fi
 
   if [ -f "$USER_SCRIPT_PATH" ]; then
     echo "Ejecutando el script..."
@@ -33,22 +28,13 @@ if ! command -v emulationstation_test &> /dev/null; then
     exit 1
   fi
 else
-  echo "emulationstation está instalado. Clonando el repositorio en $GLOBAL_INSTALL_DIR..."
   rm -rf "$GLOBAL_INSTALL_DIR"
   git clone "$REPO_URL_FULL" "$GLOBAL_INSTALL_DIR"
-  if [ $? -ne 0 ]; then
-    echo "Error al clonar el repositorio en $GLOBAL_INSTALL_DIR."
-    exit 1
-  fi
-
   SCRIPT_PATH="$GLOBAL_INSTALL_DIR/scripts/menu-super-retropie.sh"
   if [ -f "$SCRIPT_PATH" ]; then
     echo "Ejecutando el script de menú..."
     chmod +x "$SCRIPT_PATH"
     exec "$SCRIPT_PATH" "$@"
-  else
-    echo "Error: $SCRIPT_PATH no existe."
-    exit 1
   fi
 fi
 
@@ -64,9 +50,6 @@ install_if_missing() {
 
 # Verificar e instalar dependencias necesarias
 install_if_missing dialog
-install_if_missing wget
-install_if_missing git
-install_if_missing lvm2
 install_if_missing expect
 
 # Función para comprobar si el volumen lógico está usando todo el espacio disponible
