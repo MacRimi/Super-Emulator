dcab26311a7878a815c3b9fd8aeac3c3100dedbb
MacRimi <123239993+MacRimi@users.noreply.github.com>
2024-05-22 23:34:27 +0200

    Update test-1.sh

--- scripts/test-1.sh
+++ scripts/test-1.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
-# Función para obtener el nombre del juego usando la API de GameTDB
+# Obtener el directorio del script
+script_dir=$(dirname "$(realpath "$0")")
+
+# Archivo de la base de datos
+db_file="$script_dir/wiitdb.xml"  # Especifica la ruta completa al archivo de la base de datos local
+
+# Verificar si el archivo de la base de datos existe
+if [ ! -f "$db_file" ]; then
+  echo "Database file not found! Please ensure $db_file is in the same directory as the script."
+  exit 1
+fi
+
+# Función para obtener el nombre del juego desde la base de datos local
 get_game_name() {
   local serial="$1"
-  local url="https://www.gametdb.com/api/GetGame.php?type=PS3&serial=$serial"
-  local response=$(curl -s "$url")
-  echo "Response from GameTDB for serial $serial: $response" # Línea de depuración
-  local game_name=$(echo "$response" | grep -oPm1 "(?<=<title>)[^<]+")
+  
+  # Extraer el nombre del juego en español usando xmlstarlet
+  local game_name=$(xmlstarlet sel -t -m "//game[id='$serial']/locale[@lang='ES']" -v 'title' -n "$db_file")
+  
+  if [ -z "$game_name" ]; then
+    echo "No game name found for serial $serial"
+  else
+    echo "Game name found: $game_name"
+  fi
   echo "$game_name"
 }
 
@@ -21,8 +38,14 @@ for game_dir in "$rpcs3_game_dir"/*; do
   if [ -d "$game_dir" ]; then
     # Obtener el serial del juego (nombre del subdirectorio)
     serial=$(basename "$game_dir")
+    
+    # Verificar que el serial no contenga caracteres inesperados
+    if [[ "$serial" =~ [^a-zA-Z0-9_] ]]; then
+      echo "Serial inválido: $serial"
+      continue
+    fi
 
-    # Obtener el nombre del juego usando la API de GameTDB
+    # Obtener el nombre del juego usando la base de datos local
     game_name=$(get_game_name "$serial")
 
     # Si no se encuentra el nombre del juego, usar el serial como nombre
@@ -32,7 +55,7 @@ for game_dir in "$rpcs3_game_dir"/*; do
     fi
 
     # Crear el nombre del enlace simbólico
-    symlink_name="$retropie_roms_dir/$game_name.PS3/PS3_GAME"
+    symlink_name="$retropie_roms_dir/${game_name// /_}.PS3/PS3_GAME"
 
     # Crear el directorio destino si no existe
     mkdir -p "$(dirname "$symlink_name")"
