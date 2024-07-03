ab70c243c6aae06dc45809599f3efab6ec856476
MacRimi <123239993+MacRimi@users.noreply.github.com>
2024-07-03 18:15:25 +0200

    Add files via upload

---
+++ steam/steam.sh
@@ -0,0 +1,147 @@
+#!/bin/bash
+
+# Get the machine hardware name
+architecture=$(uname -m)
+
+# Check if the architecture is x86_64 (AMD/Intel)
+if [ "$architecture" != "x86_64" ]; then
+    echo "This script only runs on AMD or Intel (x86_64) CPUs, not on $architecture."
+    exit 1
+fi
+
+
+
+# Define the options
+OPTIONS=("1" "Install STABLE: Download prebuilt Arch Container (RECOMMENDED)"
+         "2" "Install UNSTABLE: Build Container from scratch (ARCH MIRRORS CAN FAIL!)"
+         "3" "List all Packages in Container"
+         "4" "Uninstall Arch Container"
+         "5" "Update ES Launcher shortcuts for Arch container"
+         "6" "Re-download prebuilt container"
+         "7" "Container Update (MIRRORS CAN FAIL/~45-90min/30GB free space needed)"
+         "8" "Addon: DESKTOP/WINDOWED Mode (Experimental)"
+         "9" "Addon: Add/Update Lutris Menu & Shortcuts to Emulationstation"
+         "10" "Addon: Add/Update Heroic Menu & Shortcuts to Emulationstation"
+         "11" "Addon: Emudeck (experimental)"
+         "12" "Addon: Nativefier (turn websites into apps and add to ES Menu)"
+         "13" "Addon: NVIDIA CUDA")
+
+# Display the dialog and get the user choice
+CHOICE=$(dialog --clear --backtitle "Arch Container Management" \
+                --title "Main Menu" \
+                --menu "Choose an option:" 20 90 3 \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+
+# Act based on the user choice
+case $CHOICE in
+    1)
+        echo "Installing Steam Container..."
+        rm /tmp/runner 2>/dev/null
+        wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O /tmp/runner https://raw.githubusercontent.com/uureel/batocera.pro/main/steam/install2.sh
+        dos2unix /tmp/runner 2>/dev/null 
+        chmod 777 /tmp/runner 2>/dev/null
+        bash /tmp/runner
+        ;;
+     2)
+        echo "Installing Steam Container..."
+        rm /tmp/runner 2>/dev/null
+        wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O /tmp/runner https://raw.githubusercontent.com/uureel/batocera.pro/main/steam/install_new.sh
+        dos2unix /tmp/runner 2>/dev/null 
+        chmod 777 /tmp/runner 2>/dev/null
+        bash /tmp/runner
+        ;;
+     3)
+        echo "Loading Package List..."
+        rm /tmp/runner 2>/dev/null
+        wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O /tmp/runner https://raw.githubusercontent.com/uureel/batocera.pro/main/steam/list.sh
+        dos2unix /tmp/runner 2>/dev/null 
+        chmod 777 /tmp/runner 2>/dev/null
+        bash /tmp/runner
+        ;;
+    4)
+        echo "Loading Uninstall script..."
+        rm /tmp/runner 2>/dev/null
+        wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O /tmp/runner https://raw.githubusercontent.com/uureel/batocera.pro/main/steam/uninstall.sh
+        dos2unix /tmp/runner 2>/dev/null 
+        chmod 777 /tmp/runner 2>/dev/null
+        bash /tmp/runner
+        ;;
+    5)  
+        echo "Update EmulationStation Arch Container Launcher Shortcuts..."
+        rm /tmp/runner 2>/dev/null
+        wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O /tmp/runner https://raw.githubusercontent.com/uureel/batocera.pro/main/steam/update_shortcuts.sh
+        dos2unix /tmp/runner 2>/dev/null 
+        chmod 777 /tmp/runner 2>/dev/null
+        bash /tmp/runner
+        ;;    
+    6)  
+        echo "Update/Re-download Container..."
+        rm /tmp/runner 2>/dev/null
+        wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O /tmp/runner https://raw.githubusercontent.com/uureel/batocera.pro/main/steam/redownload.sh
+        dos2unix /tmp/runner 2>/dev/null 
+        chmod 777 /tmp/runner 2>/dev/null
+        bash /tmp/runner
+        ;;
+    7)  
+        echo "Update Conty Container..."
+        rm /tmp/runner 2>/dev/null
+        wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O /tmp/runner https://raw.githubusercontent.com/uureel/batocera.pro/main/steam/build.sh
+        dos2unix /tmp/runner 2>/dev/null 
+        chmod 777 /tmp/runner 2>/dev/null
+        bash /tmp/runner
+        ;;
+     8)  
+        echo "Installing Desktop/Windowed Mode..."
+        rm /tmp/runner 2>/dev/null
+        wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O /tmp/runner https://github.com/uureel/batocera.pro/raw/main/steam/arch-de.sh
+        dos2unix /tmp/runner 2>/dev/null 
+        chmod 777 /tmp/runner 2>/dev/null
+        bash /tmp/runner
+        ;;
+    9)  
+        echo "Add/Update Lutris shortcuts to emulationstation..."
+        rm /tmp/runner 2>/dev/null
+        wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O /tmp/runner https://raw.githubusercontent.com/uureel/batocera.pro/main/steam/addon_lutris.sh
+        dos2unix /tmp/runner 2>/dev/null 
+        chmod 777 /tmp/runner 2>/dev/null
+        bash /tmp/runner
+        ;;
+    10)  
+        echo "Add/update Heroic shortcuts to emulationstation..."
+        rm /tmp/runner 2>/dev/null
+        wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O /tmp/runner https://raw.githubusercontent.com/uureel/batocera.pro/main/steam/addon_heroic.sh
+        dos2unix /tmp/runner 2>/dev/null 
+        chmod 777 /tmp/runner 2>/dev/null
+        bash /tmp/runner
+        ;;
+    11)  
+        echo "Emudeck Menu..."
+        rm /tmp/runner 2>/dev/null
+        wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O /tmp/runner https://github.com/uureel/batocera.pro/raw/main/emudeck/emudeck.sh
+        dos2unix /tmp/runner 2>/dev/null 
+        chmod 777 /tmp/runner 2>/dev/null
+        bash /tmp/runner
+        ;;
+    12)  
+        echo "Webapps Installer..."
+        rm /tmp/runner 2>/dev/null
+        wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O /tmp/runner https://github.com/uureel/batocera.pro/raw/main/webapps/install.sh
+        dos2unix /tmp/runner 2>/dev/null 
+        chmod 777 /tmp/runner 2>/dev/null
+        bash /tmp/runner
+        ;;
+    13)  
+        echo "NVIDIA CUDA Installer..."
+        rm /tmp/runner 2>/dev/null
+        wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O /tmp/runner https://raw.githubusercontent.com/uureel/batocera.pro/main/steam/build/cuda.sh
+        dos2unix /tmp/runner 2>/dev/null 
+        chmod 777 /tmp/runner 2>/dev/null
+        bash /tmp/runner
+        ;;
+    *)
+        echo "No valid option selected or cancelled. Exiting."
+        ;;
+esac
