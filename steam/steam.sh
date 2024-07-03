51326e7457a41dd14f87ce3dd3720a1fbd8b35db
MacRimi <123239993+MacRimi@users.noreply.github.com>
2024-07-03 22:53:01 +0200

    Update steam.sh

--- steam/steam.sh
+++ steam/steam.sh
@@ -14,18 +14,8 @@ REPO_BASE_URL="${REPO_BASE_URL:-https://raw.githubusercontent.com/MacRimi/Super-
 
 # Define the options
 OPTIONS=("1" "Install STABLE: Download prebuilt Arch Container (RECOMMENDED)"
-         "2" "Install UNSTABLE: Build Container from scratch (ARCH MIRRORS CAN FAIL!)"
-         "3" "List all Packages in Container"
-         "4" "Uninstall Arch Container"
-         "5" "Update ES Launcher shortcuts for Arch container"
-         "6" "Re-download prebuilt container"
-         "7" "Container Update (MIRRORS CAN FAIL/~45-90min/30GB free space needed)"
-         "8" "Addon: DESKTOP/WINDOWED Mode (Experimental)"
-         "9" "Addon: Add/Update Lutris Menu & Shortcuts to Emulationstation"
-         "10" "Addon: Add/Update Heroic Menu & Shortcuts to Emulationstation"
-         "11" "Addon: Emudeck (experimental)"
-         "12" "Addon: Nativefier (turn websites into apps and add to ES Menu)"
-         "13" "Addon: NVIDIA CUDA")
+         "2" "Uninstall Arch Container"
+         "3" "Update ES Launcher shortcuts for Arch container")
 
 # Display the dialog and get the user choice
 CHOICE=$(dialog --clear --backtitle "Arch Container Management" \
@@ -46,23 +36,7 @@ case $CHOICE in
         chmod 777 /tmp/runner 2>/dev/null
         bash /tmp/runner
         ;;
-     2)
-        echo "Installing Steam Container..."
-        rm /tmp/runner 2>/dev/null
-        wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O /tmp/runner "${REPO_BASE_URL}/install_new.sh"
-        dos2unix /tmp/runner 2>/dev/null 
-        chmod 777 /tmp/runner 2>/dev/null
-        bash /tmp/runner
-        ;;
-     3)
-        echo "Loading Package List..."
-        rm /tmp/runner 2>/dev/null
-        wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O /tmp/runner "${REPO_BASE_URL}/list.sh"
-        dos2unix /tmp/runner 2>/dev/null 
-        chmod 777 /tmp/runner 2>/dev/null
-        bash /tmp/runner
-        ;;
-    4)
+    2)
         echo "Loading Uninstall script..."
         rm /tmp/runner 2>/dev/null
         wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O /tmp/runner "${REPO_BASE_URL}/uninstall.sh"
@@ -70,7 +44,7 @@ case $CHOICE in
         chmod 777 /tmp/runner 2>/dev/null
         bash /tmp/runner
         ;;
-    5)  
+    3)  
         echo "Update EmulationStation Arch Container Launcher Shortcuts..."
         rm /tmp/runner 2>/dev/null
         wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O /tmp/runner "${REPO_BASE_URL}/update_shortcuts.sh"
@@ -78,70 +52,6 @@ case $CHOICE in
         chmod 777 /tmp/runner 2>/dev/null
         bash /tmp/runner
         ;;    
-    6)  
-        echo "Update/Re-download Container..."
-        rm /tmp/runner 2>/dev/null
-        wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O /tmp/runner "${REPO_BASE_URL}/redownload.sh"
-        dos2unix /tmp/runner 2>/dev/null 
-        chmod 777 /tmp/runner 2>/dev/null
-        bash /tmp/runner
-        ;;
-    7)  
-        echo "Update Conty Container..."
-        rm /tmp/runner 2>/dev/null
-        wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O /tmp/runner "${REPO_BASE_URL}/build.sh"
-        dos2unix /tmp/runner 2>/dev/null 
-        chmod 777 /tmp/runner 2>/dev/null
-        bash /tmp/runner
-        ;;
-     8)  
-        echo "Installing Desktop/Windowed Mode..."
-        rm /tmp/runner 2>/dev/null
-        wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O /tmp/runner "${REPO_BASE_URL}/arch-de.sh"
-        dos2unix /tmp/runner 2>/dev/null 
-        chmod 777 /tmp/runner 2>/dev/null
-        bash /tmp/runner
-        ;;
-    9)  
-        echo "Add/Update Lutris shortcuts to emulationstation..."
-        rm /tmp/runner 2>/dev/null
-        wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O /tmp/runner "${REPO_BASE_URL}/addon_lutris.sh"
-        dos2unix /tmp/runner 2>/dev/null 
-        chmod 777 /tmp/runner 2>/dev/null
-        bash /tmp/runner
-        ;;
-    10)  
-        echo "Add/update Heroic shortcuts to emulationstation..."
-        rm /tmp/runner 2>/dev/null
-        wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O /tmp/runner "${REPO_BASE_URL}/addon_heroic.sh"
-        dos2unix /tmp/runner 2>/dev/null 
-        chmod 777 /tmp/runner 2>/dev/null
-        bash /tmp/runner
-        ;;
-    11)  
-        echo "Emudeck Menu..."
-        rm /tmp/runner 2>/dev/null
-        wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O /tmp/runner "${REPO_BASE_URL}/emudeck/emudeck.sh"
-        dos2unix /tmp/runner 2>/dev/null 
-        chmod 777 /tmp/runner 2>/dev/null
-        bash /tmp/runner
-        ;;
-    12)  
-        echo "Webapps Installer..."
-        rm /tmp/runner 2>/dev/null
-        wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O /tmp/runner "${REPO_BASE_URL}/webapps/install.sh"
-        dos2unix /tmp/runner 2>/dev/null 
-        chmod 777 /tmp/runner 2>/dev/null
-        bash /tmp/runner
-        ;;
-    13)  
-        echo "NVIDIA CUDA Installer..."
-        rm /tmp/runner 2>/dev/null
-        wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O /tmp/runner "${REPO_BASE_URL}/build/cuda.sh"
-        dos2unix /tmp/runner 2>/dev/null 
-        chmod 777 /tmp/runner 2>/dev/null
-        bash /tmp/runner
-        ;;
     *)
         echo "No valid option selected or cancelled. Exiting."
         ;;
