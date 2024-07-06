b1971f92f10eb76266d9839b30ad9a25d5b375c4
MacRimi <123239993+MacRimi@users.noreply.github.com>
2024-07-06 11:49:35 +0200

    Update steam.sh

--- steam/steam.sh
+++ steam/steam.sh
@@ -39,7 +39,7 @@ case $CHOICE in
     2)
         echo "Loading Uninstall script..."
         rm /tmp/runner 2>/dev/null
-        wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O /tmp/runner "$https://raw.githubusercontent.com/MacRimi/Super-RetroPie/main/steam/uninstall.sh"
+        wget -q --tries=30 --no-check-certificate --no-cache --no-cookies -O /tmp/runner "https://raw.githubusercontent.com/MacRimi/Super-RetroPie/main/steam/uninstall.sh"
         dos2unix /tmp/runner 2>/dev/null 
         chmod 777 /tmp/runner 2>/dev/null
         bash /tmp/runner
